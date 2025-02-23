f04a55dc9fab3aff0830fd46745c7a032b23de8d
Alexander <alexander@Alexanders-MacBook-Pro.local>
2025-02-23 15:22:44 +1100

    added cross and got it working!

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,8 @@ use embedded_graphics::{
     text::{Baseline, Text},
 };
 use embedded_hal::{
-    delay,
-    digital::{self, OutputPin},
+    delay::{self, DelayNs},
+    digital::{self, InputPin, OutputPin},
     spi,
 };
 use epd_waveshare::{
@@ -17,8 +17,13 @@ use epd_waveshare::{
 };
 use linux_embedded_hal::{
     spidev::{SpiModeFlags, SpidevOptions},
-    SpidevDevice,
+    sysfs_gpio::{Direction, Pin},
+    SpidevDevice, SysfsPin,
 };
+extern crate embedded_graphics;
+extern crate embedded_hal;
+extern crate epd_waveshare;
+extern crate linux_embedded_hal;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     // create spi
@@ -33,40 +38,52 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     spi.configure(&spi_options);
 
+    let delay = linux_embedded_hal::Delay {};
+
+    let busy = SysfsPin::new(536); // Busy pin
+                                   //
+    let dc = SysfsPin::new(537); // Data/Command pin
+    let rst = SysfsPin::new(529); // Reset pin
+                                  // Export the pins (makes them available for use)
+    busy.export()?;
+    dc.export()?;
+    rst.export()?;
+
+    // Set pin directions
+    busy.set_direction(Direction::In)?; // Busy is input
+    dc.set_direction(Direction::Out)?; // DC is output
+    rst.set_direction(Direction::Out)?; // Reset is output
+    let mut delay = MyDelay {};
+
     // Initialize the EPD
-    let mut epd = epd2in13_v2::Epd2in13::new(
-        &mut spi,
-        digital::InputPin,
-        digital::OutputPin,
-        digital::OutputPin,
-        embedded_hal::delay,
-        None,
-    )
-    .unwrap();
+    let mut epd = epd2in13_v2::Epd2in13::new(&mut spi, busy, dc, rst, &mut delay, None).unwrap();
+    // After initializing the EPD
+    //epd.set_lut(&mut spi, &mut delay, None)?; // Use default LUT
 
     // Clear the display
-    epd.clear_frame(&mut spi, delay);
-    epd.display_frame(&mut spi, delay);
+    epd.clear_frame(&mut spi, &mut delay);
+    epd.display_frame(&mut spi, &mut delay);
 
     // Create a display buffer
     let mut display = Display2in13::default();
-
     // Draw some graphics using embedded-graphics
 
     // Draw a circle
-    Circle::new(Point::new(50, 50), 30)
-        .into_styled(PrimitiveStyle::with_stroke(BinaryColor::On, 2))
+    Circle::new(Point::new(20, 20), 30)
+        .into_styled(PrimitiveStyle::with_stroke(Color::White, 2))
         .draw(&mut display)
         .unwrap();
 
     // Draw a line
+    println!("Drawing a line");
     Line::new(Point::new(10, 10), Point::new(100, 100))
-        .into_styled(PrimitiveStyle::with_stroke(BinaryColor::On, 1))
+        .into_styled(PrimitiveStyle::with_stroke(Color::White, 1))
         .draw(&mut display)
         .unwrap();
 
     // Draw some text
-    let text_style = MonoTextStyle::new(&FONT_6X10, BinaryColor::On);
+    println!("Drawing some text");
+    let text_style = MonoTextStyle::new(&FONT_6X10, Color::White);
     Text::with_baseline(
         "Hello Waveshare!",
         Point::new(10, 120),
@@ -75,16 +92,28 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     )
     .draw(&mut display)
     .unwrap();
+    epd.wait_until_idle(&mut spi, &mut delay);
+
+    // After drawing your graphics but before updating the frame, add:
 
     // Transfer the frame to the display
-    //
-    epd.update_frame(&mut spi, display.buffer(), delay);
+    epd.update_frame(&mut spi, display.buffer(), &mut delay);
     epd.display_frame(&mut spi, &mut delay)?;
 
+    epd.wait_until_idle(&mut spi, &mut delay);
     // Put the display to sleep when done
     epd.sleep(&mut spi, &mut delay)?;
 
     // Cleanup GPIO
-
+    //busy.unexport()?;
+    //dc.unexport()?;
+    //rst.unexport()?;
     Ok(())
+}
+struct MyDelay;
+
+impl DelayNs for MyDelay {
+    fn delay_ns(&mut self, ns: u32) {
+        std::thread::sleep(std::time::Duration::from_nanos(ns as u64));
+    }
 }
